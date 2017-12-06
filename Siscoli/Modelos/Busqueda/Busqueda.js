d1c5d8a2f859f3607b768478867fade469c90e83
elkrlos10 <www.kliche10@gmail.com>
2017-12-06 10:46:05 -0500

    Initial commit

---
+++ Siscoli/Modelos/Busqueda/Busqueda.js
@@ -0,0 +1,5 @@
+﻿SiscoliApp.controller('BusquedaController',
+    ['$scope', '$rootScope', '$location', '$cookies', '$cookieStore', '$routeParams', '$sce',
+        function ($scope, $rootScope, $location, $cookies, $cookieStore, $routeParams, $sce) {
+
+        }]);
---
+++ Siscoli/Modelos/Login/Login.js
@@ -0,0 +1,9 @@
+﻿SiscoliApp.controller('LoginController',
+    ['$scope', '$rootScope', '$location','$cookies', '$cookieStore', '$routeParams', '$sce',
+        function ($scope, $rootScope, $location, $cookies, $cookieStore, $routeParams, $sce) {
+
+            $("#MenuPrincipal").css("display", "none");
+
+
+
+        }]);
